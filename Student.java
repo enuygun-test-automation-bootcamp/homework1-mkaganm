758c5e91f9453ae20777b45ca3e2c01ead0f9365
Mustafa Kağan Meriç <mustafakaganmeric@gmail.com>
2022-06-25 22:54:18 +0300

    abstract class is added

---
+++ Student.java
@@ -0,0 +1,22 @@
+public class Student extends Bootcamp {
+
+    int[] points;
+
+    // yapıcı fonksiyon
+    public Student(String name, String surname, String email) {
+        super(name, surname, email);
+    }
+
+    public void askQuestion(String question) {
+        System.out.println("Asking ?" + question);
+    }
+
+    public void silence() {
+        System.out.println("........");
+    }
+
+    @Override
+    public void homework(String homeworkInfo) {
+        System.out.println("Student is doing homework..." + homeworkInfo);
+    }
+}

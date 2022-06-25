1064e51ca155cd398d38b0e5323da9a603f1f7c3
Mustafa Kağan Meriç <mustafakaganmeric@gmail.com>
2022-06-25 23:03:40 +0300

    abstract class is added.Subclasses are added.

--- Main.java
+++ Main.java
@@ -1,6 +1,34 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("hello bootcamp");
-        System.out.println("changes");
+        System.out.println("M.K.M");
+
+        Teacher hhal = new Teacher("Hasan Hüseyin Ali", "Gül", "hhaligul@patika.com");
+        Assistant assistant = new Assistant("Patika", "Dev", "patika@patika.com");
+        Student mkm = new Student("Mustafa Kağan", "MERİÇ", "mustafakaganmeric@gmail.com");
+
+        hhal.connectMeeting();
+        assistant.connectMeeting();
+        mkm.connectMeeting();
+
+        assistant.attendance();
+
+        hhal.askQuestion();
+        mkm.silence();
+
+        String question = "???";
+        mkm.askQuestion(question);
+        hhal.answerQuestion(question);
+
+        String homeworkInfo = "homework1";
+        hhal.homework(homeworkInfo);
+
+        hhal.exitMeeting();
+        mkm.exitMeeting();
+        assistant.exitMeeting();
+
+        mkm.homework(homeworkInfo);
+        assistant.homework(homeworkInfo);
+
     }
 }
+

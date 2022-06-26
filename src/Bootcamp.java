ffa2d67d351c85aa435aec457d232b1c43bc09ba
Mustafa Kağan Meriç <mustafakaganmeric@gmail.com>
2022-06-26 13:05:02 +0300

    files into src

--- src/Bootcamp.java
+++ src/Bootcamp.java
@@ -1,3 +1,5 @@
+package src;
+
 public abstract class Bootcamp {
 
     private String name;
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+package src;
+
+import src.Teacher;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("M.K.M");
